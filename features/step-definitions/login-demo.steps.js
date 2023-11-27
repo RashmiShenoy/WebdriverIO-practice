bb191afb097bcc713ed6c10bf58aba5081f51b8a
Rashmi <shenoyrashmi21@gmail.com>
2023-11-27 16:30:35 +0530

    Completed project run with parameterized creds and login

---
+++ features/step-definitions/login-demo.steps.js
@@ -0,0 +1,28 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { expect, $ } = require('@wdio/globals')
+
+Given(/^user is on login page$/, async () => {
+    await browser.url('https://the-internet.herokuapp.com/login')
+});
+
+// When(/^user enters username and password$/, async () => {
+//     await $('#username').setValue('tomsmith')
+//     await $('#password').setValue('SuperSecretPassword!')
+// });
+
+When(/^user enters (.*) and (.*)$/, async (username, password) => {
+    await $('#username').setValue(username)
+    await $('#password').setValue(password)
+});
+
+When(/^clicks on login button$/, async () => {
+    await $('button[type="submit"]').click()
+});
+
+// Then (/^user is navigated to the homepage$/, async() =>{
+//     await expect($('#flash')).toHaveTextContaining('You logged into a secure area!')
+// });
+
+Then(/^this (.*) is displayed$/, async (message) => {
+	await expect($('#flash')).toHaveTextContaining(message)
+});
